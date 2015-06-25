b1090001ef0a486633e9597f8beb9c729cf40e22
EyrePreston <prestoneyre12@gmail.com>
2015-06-25 15:01:48 -0600

    New Page

--- WebApp/WebApp/Default.aspx.designer.cs
+++ WebApp/WebApp/Default.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace WebApp {
     public partial class _Default {
         
         /// <summary>
-        /// GridView1 control.
+        /// ListView1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.ListView ListView1;
         
         /// <summary>
         /// SqlDataSource1 control.
